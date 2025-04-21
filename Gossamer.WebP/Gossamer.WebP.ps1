fa5458682a2a3d06fee5daa29bf398064d0471bf
Joonas Anttonen <joonas.anttonen@outlook.com>
2025-04-21 08:23:35 +0300

    Add Gossamer.WebP output copy to final destination

--- Gossamer.WebP/Gossamer.WebP.ps1
+++ Gossamer.WebP/Gossamer.WebP.ps1
@@ -15,21 +15,21 @@ function Exit-OnFailed {
 Set-Location ./libwebp
 
 & cmake -S . `
-        -B build `
-        -A x64 `
-        -DCMAKE_BUILD_TYPE=Release `
-        -DWEBP_BUILD_ANIM_UTILS=OFF `
-        -DWEBP_BUILD_CWEBP=OFF `
-        -DWEBP_BUILD_DWEBP=OFF `
-        -DWEBP_BUILD_GIF2WEBP=OFF `
-        -DWEBP_BUILD_IMG2WEBP=OFF `
-        -DWEBP_BUILD_VWEBP=OFF `
-        -DWEBP_BUILD_WEBPINFO=OFF `
-        -DWEBP_BUILD_LIBWEBPMUX=OFF `
-        -DWEBP_BUILD_WEBPMUX=OFF `
-        -DWEBP_BUILD_EXTRAS=OFF `
-        -DWEBP_BUILD_WEBP_JS=OFF `
-        -DWEBP_BUILD_FUZZTEST=OFF
+    -B build `
+    -A x64 `
+    -DCMAKE_BUILD_TYPE=Release `
+    -DWEBP_BUILD_ANIM_UTILS=OFF `
+    -DWEBP_BUILD_CWEBP=OFF `
+    -DWEBP_BUILD_DWEBP=OFF `
+    -DWEBP_BUILD_GIF2WEBP=OFF `
+    -DWEBP_BUILD_IMG2WEBP=OFF `
+    -DWEBP_BUILD_VWEBP=OFF `
+    -DWEBP_BUILD_WEBPINFO=OFF `
+    -DWEBP_BUILD_LIBWEBPMUX=OFF `
+    -DWEBP_BUILD_WEBPMUX=OFF `
+    -DWEBP_BUILD_EXTRAS=OFF `
+    -DWEBP_BUILD_WEBP_JS=OFF `
+    -DWEBP_BUILD_FUZZTEST=OFF
 Exit-OnFailed "libwebp configuration failed"
 
 & cmake --build build --config Release --verbose
@@ -41,10 +41,10 @@ Exit-OnFailed "libwebp install failed"
 Set-Location ../
 
 & cmake -S . `
-        -B build `
-        -A x64 `
-        -DCMAKE_BUILD_TYPE=Release `
-        -DWebP_DIR=libwebp/build/install/share/WebP/cmake
+    -B build `
+    -A x64 `
+    -DCMAKE_BUILD_TYPE=Release `
+    -DWebP_DIR=libwebp/build/install/share/WebP/cmake
 Exit-OnFailed "Gossamer.WebP configuration failed"
 
 & cmake --build build --config Release --verbose
@@ -52,3 +52,15 @@ Exit-OnFailed "Gossamer.WebP build failed"
 
 & cmake --install build --config Release --prefix build/install --verbose
 Exit-OnFailed "Gossamer.WebP install failed"
+
+$SrcDir = Resolve-Path -Path "build/install"
+$DstDir = Resolve-Path -Path "../bin"
+New-Item -ItemType Directory -Path $DstDir -Force | Out-Null
+
+Write-Host "Copied to $DstDir" -ForegroundColor Green
+
+Get-ChildItem -Path $SrcDir -Filter "*.dll" -Recurse | ForEach-Object {
+    Copy-Item -Path $_.FullName -Destination $DstDir -Force
+    Write-Host "$($_.Name)" -ForegroundColor Green
+}
+
