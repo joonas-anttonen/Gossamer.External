81d9635add02f4b30685a3656c9092f20996dac9
Joonas Anttonen <joonas.anttonen@outlook.com>
2025-04-21 08:00:26 +0300

    Add Gossamer.WebP

---
+++ Gossamer.WebP/Gossamer.WebP.cpp
@@ -0,0 +1,105 @@
+#include <webp/decode.h>
+#include <webp/encode.h>
+
+enum class Status
+{
+    OK = 0,
+    InvalidArgument = 1,
+    InvalidData = 2,
+};
+
+enum class Format
+{
+    RGBA = 0,
+};
+
+extern "C"
+{
+    __declspec(dllexport) int32_t Analyze(const uint8_t *in_data, size_t in_data_size, int32_t *width, int32_t *height, int32_t *has_alpha)
+    {
+        if (in_data == nullptr || in_data_size == 0 || width == nullptr || height == nullptr || has_alpha == nullptr)
+            return static_cast<int32_t>(Status::InvalidArgument);
+
+        WebPBitstreamFeatures features;
+        if (WebPGetFeatures(in_data, in_data_size, &features) == VP8_STATUS_OK)
+        {
+            *width = features.width;
+            *height = features.height;
+            *has_alpha = features.has_alpha;
+            return static_cast<int32_t>(Status::OK);
+        }
+        else
+        {
+            *width = 0;
+            *height = 0;
+            *has_alpha = 0;
+            return static_cast<int32_t>(Status::InvalidData);
+        }
+    }
+
+    __declspec(dllexport) int32_t Decode(const uint8_t *in_data, size_t in_data_size, Format out_format, uint8_t **out_data, size_t *out_data_size)
+    {
+        if (in_data == nullptr || in_data_size == 0 || out_data == nullptr || out_data_size == nullptr)
+            return static_cast<int32_t>(Status::InvalidArgument);
+
+        int width = 0, height = 0;
+        if (out_format == Format::RGBA)
+        {
+            *out_data = WebPDecodeRGBA(in_data, in_data_size, &width, &height);
+        }
+        else
+        {
+            out_data = nullptr;
+            *out_data_size = 0;
+            return static_cast<int32_t>(Status::InvalidArgument);
+        }
+
+        if (out_data != nullptr)
+        {
+            *out_data_size = width * height * 4;
+            return static_cast<int32_t>(Status::OK);
+        }
+        else
+        {
+            *out_data_size = 0;
+            return static_cast<int32_t>(Status::InvalidData);
+        }
+    }
+
+    __declspec(dllexport) int32_t Encode(const uint8_t *in_data, size_t in_data_size, Format in_format, int width, int height, uint8_t **out_data, size_t *out_data_size)
+    {
+        if (in_data == nullptr || in_data_size == 0 || width <= 0 || height <= 0 || out_data == nullptr || out_data_size == nullptr)
+            return static_cast<int32_t>(Status::InvalidArgument);
+
+        if (in_format == Format::RGBA)
+        {
+            int stride = width * 4; // RGBA has 4 bytes per pixel
+            
+            size_t encoded = WebPEncodeLosslessRGBA(in_data, width, height, stride, out_data);
+            if (encoded > 0)
+            {
+                *out_data_size = encoded;
+                return static_cast<int32_t>(Status::OK);
+            }
+            else
+            {
+                *out_data = nullptr;
+                *out_data_size = 0;
+                return static_cast<int32_t>(Status::InvalidData);
+            }
+        }
+
+        *out_data = nullptr;
+        *out_data_size = 0;
+        return static_cast<int32_t>(Status::InvalidArgument);
+    }
+
+    __declspec(dllexport) int32_t Free(uint8_t *ptr)
+    {
+        if (ptr == nullptr)
+            return static_cast<int32_t>(Status::InvalidArgument);
+
+        WebPFree(ptr);
+        return static_cast<int32_t>(Status::OK);
+    }
+}
