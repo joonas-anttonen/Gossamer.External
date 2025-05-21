1ca7145beeaf244e7a131fe7056fd3d227b2ec61
Anttonen Joonas <Joonas.Anttonen@pemamek.com>
2025-05-21 10:42:52 +0300

    Added Gossamer.Lua to all-build script and a new install script

---
+++ Gossamer.External.Install.ps1
@@ -0,0 +1,3 @@
+# Copy all files from ./bin to ../Gossamer/Gossamer/External
+
+Copy-Item -Path ./bin/* -Destination ../Gossamer/Gossamer/External -Recurse -Force
--- Gossamer.External.ps1
+++ Gossamer.External.ps1
@@ -1,8 +1,13 @@
 Set-Location ./Gossamer.FreeType
 & ./Gossamer.FreeType.ps1
 
+Set-Location ../Gossamer.Lua
+& ./Gossamer.Lua.ps1
+
 Set-Location ../Gossamer.Vma
 & ./Gossamer.Vma.ps1
 
 Set-Location ../Gossamer.WebP
 & ./Gossamer.WebP.ps1
+
+Set-Location ..
